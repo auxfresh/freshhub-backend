8bfb218f43824ce08b1495a461789fc1479c38da
auxfresh <freshcoindrop@gmail.com>
2025-05-17 05:58:19 +0100

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { authenticate, requireAdmin } = require('../middleware/auth');
-const User = require('../models/User');
+const User = require('../models/user');
 
 const router = express.Router();
 
